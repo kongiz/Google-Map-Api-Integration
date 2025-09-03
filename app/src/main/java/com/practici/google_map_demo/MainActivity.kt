9b77cfbb5f08ea253e148157b4d60aeb5c88a249
Haruna Kongiz <kongiraharuna@yahoo.com>
2025-09-03 11:28:31 +0530

    Initial commit

---
+++ app/src/main/java/com/practici/google_map_demo/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.practici.google_map_demo
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+
+class MainActivity : AppCompatActivity(), OnMapReadyCallback{
+    private lateinit var mMap: GoogleMap
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+
+    }
+
+    override fun onMapReady(p0: GoogleMap) {
+        mMap = p0
+
+//        val location = LatLng(23.0225, 72.5714) // example ahmedabad
+//        mMap.addMarker(MarkerOptions().position(location).title("Marker is Ahmedabad"))
+//        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(location, 12f))
+    }
+}
